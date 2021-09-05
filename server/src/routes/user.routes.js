ff8674a3e82599293fa7ad350e2ccbd0676e54f2
Atique Morshed Sami <iamsamix17@gmail.com>
2021-09-05 22:41:06 +0600

    +backend validation(email, username, password), +signup user, +generate and return JWT token

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,8 +1,24 @@
 import express from "express";
+import { check } from "express-validator";
 import { registerUserController } from "../controllers/register-user.controllers.js";
-
 const router = express.Router();
 
-router.post("/signup", registerUserController);
+router.post(
+  "/signup",
+  [
+    check("username", "Username must be at least 5 characters long.")
+      .exists()
+      .isLength({ min: 5 }),
+    check("email", "Email must be valid").exists().isEmail().normalizeEmail(),
+    check(
+      "password",
+      "Password must be at least 8 characters long and must contain atleast one number."
+    )
+      .exists()
+      .isLength(8)
+      .matches(/\d/),
+  ],
+  registerUserController
+);
 
 export default router;
